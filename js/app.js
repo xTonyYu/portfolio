e507cc5e7110e5beffeca2da2393a5efd436102c
xTonyYu <xtonyyu@gmail.com>
2020-08-10 20:15:07 -0700

    basic styling

--- js/app.js
+++ js/app.js
@@ -1,1 +1,8 @@
 console.log("Tony's portfolio")
+
+$(document).on('click', 'a[href^="#"]', function(e) {
+    e.preventDefault();
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 500);
+});
