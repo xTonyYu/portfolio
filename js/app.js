373e89826011489754d62f225ad9896e325d5961
xTonyYu <xtonyyu@gmail.com>
2020-08-12 23:37:20 -0700

    drying added porfolio data

--- js/app.js
+++ js/app.js
@@ -1,8 +1,48 @@
 console.log("Tony's portfolio")
 
+
+
 $(document).on('click', 'a[href^="#"]', function(e) {
     e.preventDefault();
     $('html, body').animate({
         scrollTop: $($.attr(this, 'href')).offset().top
     }, 500);
 });
+
+const proj = portfolio[0]
+console.log(proj)
+
+const portfolioContentHtml = portfolio.map(proj => {
+    
+    const html1 = `
+    <div class="project wrap">
+    <div class="proj-header">
+        <a href="${proj.link}">
+            <h4><span>${proj.css}: </span>${proj.title}</h4>
+        </a>
+        <div class="link">
+            <a href="${proj.github}" target="_blank"><i class="fab fa-github-square fa-2x"></i></a>
+            <a href="${proj.link}" target="_blank"><i class="fas fa-external-link-square-alt fa-2x"></a></i></a>
+        </div>
+    </div>
+        <div class="proj-card">
+            <img class="proj-img" src="${proj.img}" alt="Project image">
+            <div class="proj-desc">
+                <ul class="highlight">
+    `
+
+    const html2 = proj.skillsApplied.map(skill => {
+        return `<li>${skill}</li>`
+    }).join('')
+
+    const html3 = `
+                </ul>
+                <p>${proj.desc}</p>
+            </div>
+        </div> 
+    </div>
+    `
+    return html1 + html2 + html3
+}).join('')
+
+$('.project-list').append(portfolioContentHtml)
---
+++ js/portfolio.js
@@ -0,0 +1,38 @@
+const portfolio = [
+    {
+        css: 'align-self',
+        title: 'Project React Jamming',
+        link: '',
+        github: '',
+        img: './images/First-React.png',
+        skillsApplied: ['React', 'JavaScript', 'Spotify Web API'],
+        desc: 'Personal project (May 2020): A site that provides potential career information for UX designers, developers, and product mangers.  Main challenges included creating a responsive carousel with cards of various widths and resizable based on viewport width and a search box with type-ahead-suggestion.******** Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem reprehenderit odio reiciendis placeat earum inventore cupiditate accusamus delectus obcaecati ea? *******'
+    },
+    {
+        css: 'align-self',
+        title: 'Plum & Pear Website',
+        link: 'https://xtonyyu.github.io/website-with-custom-carousel/detail.html',
+        github: 'https://github.com/xTonyYu/website-with-custom-carousel',
+        img: './images/PlumPear.png',
+        skillsApplied: ['JavaScript', 'HTML/CSS'],
+        desc: 'Personal project (May 2020): A site that provides potential career information for UX designers, developers, and product mangers.  Main challenges included creating a responsive carousel with cards of various widths and resizable based on viewport width and a search box with type-ahead-suggestion.'
+    },
+    {
+        css: 'justify-content',
+        title: "90's Tamagotchi Game",
+        link: 'https://xtonyyu.github.io/Tamagotchi-Game/',
+        github: 'https://github.com/xTonyYu/Tamagotchi-Game',
+        img: './images/tamagotchi-night.png',
+        skillsApplied: ['JavaScript', 'jQuery', 'HTML/CSS', 'Responsive design'],
+        desc: 'A fast-pace online game inspired by 90’s Tamagotchi game.  The object of the game is to keep your Tamagotchi alive by feeding it, plying with it, and making sure it takes naps.  However, taking naps will slowly make Tamagotchi hungry and it can only be fed while it’s awake.'
+    },
+    {
+        css: 'flex',
+        title: "Full Stack Inventory Management App",
+        link: 'https://safe-citadel-60970.herokuapp.com/login',
+        github: 'https://github.com/xTonyYu/Cello-Inventory-App',
+        img: './images/cello-inv-mngt-app.png',
+        skillsApplied: ['Express', 'Node.js', 'Mongoose', 'EJS', 'Bootstrap'],
+        desc: 'Created a full stack inventory management app in and collaborated with another developer.  The app included login authentication feature, dashboard, product category index page, product detail page.  It has the ability to create, read, update and delete products using RESTful routes while keeping the code DRY.'
+    },
+]
