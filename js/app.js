d63d01734cc08f335f7c83198fd8f4d54a738032
xTonyYu <xtonyyu@gmail.com>
2020-09-01 16:46:56 -0700

    port update
    hero img
    
    finetune navbar styling

--- js/app.js
+++ js/app.js
@@ -1,25 +1,79 @@
 console.log("Tony's portfolio")
 
+// navbar toggler click event
+$('.navbar-toggler').on('click', function() {
+    this.classList.toggle('opened')    
+    $('.navbar').toggleClass('border-radius-LL')
+})
 
-
+// navbar click then slide down to the section
 $(document).on('click', 'a[href^="#"]', function(e) {
     e.preventDefault();
+    if (this.innerText!== '') {
+        this.classList.add('active')
+    }
+    $('.navbar-toggler').toggleClass('opened')
+    $('.navbar-toggler').toggleClass('collapsed')
+    $('.navbar-toggler').attr('aria-expanded', function() {
+        return this.ariaExpanded ? false : true
+    } )
+    $('.navbar-collapse').toggleClass('show')
     $('html, body').animate({
         scrollTop: $($.attr(this, 'href')).offset().top
     }, 500);
 });
 
+// highlight nav item based on page position
+function debounce(func, wait = 100, immediate = false) {
+    let timeout;
+    return function() {
+        const context = this,
+            args = arguments;
+        const later = function() {
+            timeout = null;
+            if (!immediate) {
+                func.apply(context, args);
+            }
+        };
+        var callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) {
+            func.apply(context, args);
+        }
+    };
+}
 
-const portfolioContentHtml = portfolio.map(proj => {    
+const sections = document.querySelectorAll('section')
+function highlightNavItem() {
+    sections.forEach(sec => {
+        const winBottom = window.scrollY + window.innerHeight
+        const sectionShowing = sec.offsetTop + window.innerHeight/4
+        const sectionBottom = sec.offsetTop + sec.offsetHeight + window.innerHeight/4
+        const highlightItem = winBottom >= sectionShowing
+        const sectionPassed = winBottom > sectionBottom
+        if (highlightItem && !sectionPassed) {
+            $('.nav-link').removeClass('active')
+            if (sec.id !== 'hero') {
+                $(`#${sec.id}-link`).addClass('active')
+            }
+        }
+    })
+}
+
+$(window).on('scroll', debounce(highlightNavItem) )
+
+// creating portfolio section
+const portfolioContentHtml = portfolio.map((proj, i) => {    
     const html1 = `
     <div class="project wrap">
     <div class="proj-header">
-        <a href="${proj.link}">
+        <a href="${proj.link}" target="_blank">
             <h4><span>${proj.css}: </span>${proj.title}</h4>
         </a>
         <div class="link">
             <a href="${proj.github}" target="_blank"><i class="fab fa-github-square fa-2x"></i></a>
-            <a href="${proj.link}" target="_blank"><i class="fas fa-external-link-square-alt fa-2x"></i></a>
+            <a href="${proj.link}" target="_blank" class="ext-link"><i class="fas fa-external-link-square-alt fa-2x"></i></a>
         </div>
     </div>
         <div class="proj-card">
@@ -40,6 +94,7 @@ const portfolioContentHtml = portfolio.map(proj => {
     </div>
     `
     return html1 + html2 + html3
+    
 }).join('')
 
 $('.project-list').append(portfolioContentHtml)
--- js/portfolio.js
+++ js/portfolio.js
@@ -1,15 +1,6 @@
 const portfolio = [
     {
         css: 'align-self',
-        title: 'Project React Jamming',
-        link: '',
-        github: '',
-        img: './images/First-React.png',
-        skillsApplied: ['React', 'JavaScript', 'Spotify Web API'],
-        desc: ' ******** Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem reprehenderit odio reiciendis placeat earum inventore cupiditate accusamus delectus obcaecati ea? *******'
-    },
-    {
-        css: 'align-self',
         title: 'Plum & Pear Website',
         link: 'https://xtonyyu.github.io/website-with-custom-carousel/detail.html',
         github: 'https://github.com/xTonyYu/website-with-custom-carousel',
@@ -32,7 +23,7 @@ const portfolio = [
         link: 'https://safe-citadel-60970.herokuapp.com/login',
         github: 'https://github.com/xTonyYu/Cello-Inventory-App',
         img: './images/cello-inv-mngt-app.png',
-        skillsApplied: ['Express', 'Node.js', 'Mongoose', 'EJS', 'Bootstrap'],
+        skillsApplied: ['Express', 'Node.js', 'Mongoose', 'EJS', 'Bootstrap', 'Google Chart API'],
         desc: 'Created a full stack inventory management app in and collaborated with another developer.  The app included login authentication feature, dashboard, product category index page, product detail page.  It has the ability to create, read, update and delete products using RESTful routes while keeping the code DRY.'
     },
     {
@@ -41,7 +32,7 @@ const portfolio = [
         link: 'https://arcane-plains-74508.herokuapp.com/show',
         github: 'https://github.com/xTonyYu/Skin-Care-Prod',
         img: './images/Skin-care-prod.png',
-        skillsApplied: ['Express', 'Node.js', 'Mongoose', 'Bootstrap', 'Time Management', 'Collaboration'],
+        skillsApplied: ['Express', 'Node.js', 'MongoDB/Mongoose', 'Bootstrap', 'Time Management', 'Collaboration'],
         desc: 'Built a skin-care product review site in 24 hours by collaborating with 2 designers and 2 developers.  Applied Agile principles to pivot priorities and tasks as needed to deliver the site on time and per the project and design specifications.'
     },
     {
@@ -52,5 +43,14 @@ const portfolio = [
         img: './images/Wayfayer.png',
         skillsApplied: ['Django', 'Python', 'PostgreSQL', 'JavaScript', 'Project planning'],
         desc: 'A responsive full stack travel blog site with a team of four developers.  Gathered client input and clarification on requirements and wireframe; leveraged Trello to plan sprints, minimum requirement tasks, and extra features.  Functionality included full CRUD on blogs, login authentication, sign-up auto welcome email, and custom modals.'
+    },
+    {
+        css: 'flex-grow',
+        title: "MERN Stack E-commerce Site With Inventory App",
+        link: 'https://plumpear-store.herokuapp.com/login',
+        github: 'https://github.com/xTonyYu/Plum-and-Pear-client',
+        img: './images/plumPearStore.png',
+        skillsApplied: ['React', 'Express', 'MongoDB/Mongoose', 'Node.js', 'Payment API', 'Project planning'],
+        desc: 'A responsive full stack e-commerce site that served both the customers and shop owners.  Functionality included: RESTful Node server API; full CRUD on the inventory; utilized bcrypt and JSON web token to secure authentication and authorization of login and register; customers able to save shopping cart items until next session and make purchase using Stripe payment API. <em>(ID: "customer" Password: "1234"; ID: "AdminX" Password: "2wsx")</em>'
     },
 ]
