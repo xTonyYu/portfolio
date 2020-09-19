bd808aa263385c2fc78bbd4c777968c28963b3a2
xTonyYu <xtonyyu@gmail.com>
2020-09-19 16:51:54 -0700

    added html
    easter egg

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,21 +1,21 @@
 const portfolio = [
     {
-        css: 'align-self',
-        title: 'Plum & Pear Website',
-        link: 'https://xtonyyu.github.io/website-with-custom-carousel/detail.html',
-        github: 'https://github.com/xTonyYu/website-with-custom-carousel',
-        img: './images/PlumPear.png',
-        skillsApplied: ['JavaScript', 'HTML/CSS'],
-        desc: 'Personal project (May 2020): A site that provides potential career information for UX designers, developers, and product mangers.  Main challenges included creating a responsive carousel with cards of various widths and resizable based on viewport width and a search box with type-ahead-suggestion.'
+        css: 'flex-grow',
+        title: "MERN Stack E-commerce Site With Inventory App",
+        link: 'https://plumpear-store.herokuapp.com/login',
+        github: 'https://github.com/xTonyYu/Plum-and-Pear-client',
+        img: './images/plumPearStore.png',
+        skillsApplied: ['React', 'Express', 'MongoDB/Mongoose', 'Node.js', 'Payment API', 'Project planning'],
+        desc: 'A responsive full stack e-commerce site that served both the customers and shop owners.  Functionality included: RESTful Node server API; full CRUD on the inventory; utilized bcrypt and JSON web token to secure authentication and authorization of login and register; customers able to save shopping cart items until next session and make purchase using Stripe payment API. <em>(ID: "customer" Password: "1234"; ID: "AdminX" Password: "2wsx")</em>'
     },
     {
-        css: 'justify-content',
-        title: "90's Tamagotchi Game",
-        link: 'https://xtonyyu.github.io/Tamagotchi-Game/',
-        github: 'https://github.com/xTonyYu/Tamagotchi-Game',
-        img: './images/tamagotchi-night.png',
-        skillsApplied: ['JavaScript', 'jQuery', 'HTML/CSS', 'Responsive design'],
-        desc: 'A fast-pace online game inspired by 90’s Tamagotchi game.  The object of the game is to keep your Tamagotchi alive by feeding it, plying with it, and making sure it takes naps.  However, taking naps will slowly make Tamagotchi hungry and it can only be fed while it’s awake.'
+        css: 'flex-grow',
+        title: "Full Stack Django Travel Blog Site",
+        link: 'https://wayfayer2020.herokuapp.com/',
+        github: 'https://github.com/xTonyYu/Wayfayer',
+        img: './images/Wayfayer.png',
+        skillsApplied: ['Django', 'Python', 'PostgreSQL', 'JavaScript', 'Project planning'],
+        desc: 'A responsive full stack travel blog site with a team of four developers.  Gathered client input and clarification on requirements and wireframe; leveraged Trello to plan sprints, minimum requirement tasks, and extra features.  Functionality included full CRUD on blogs, login authentication, sign-up auto welcome email, and custom modals.'
     },
     {
         css: 'flex',
@@ -36,21 +36,22 @@ const portfolio = [
         desc: 'Built a skin-care product review site in 24 hours by collaborating with 2 designers and 2 developers.  Applied Agile principles to pivot priorities and tasks as needed to deliver the site on time and per the project and design specifications.'
     },
     {
-        css: 'flex-grow',
-        title: "Full Stack Django Travel Blog Site",
-        link: 'https://wayfayer2020.herokuapp.com/',
-        github: 'https://github.com/xTonyYu/Wayfayer',
-        img: './images/Wayfayer.png',
-        skillsApplied: ['Django', 'Python', 'PostgreSQL', 'JavaScript', 'Project planning'],
-        desc: 'A responsive full stack travel blog site with a team of four developers.  Gathered client input and clarification on requirements and wireframe; leveraged Trello to plan sprints, minimum requirement tasks, and extra features.  Functionality included full CRUD on blogs, login authentication, sign-up auto welcome email, and custom modals.'
+        css: 'align-self',
+        title: 'Plum & Pear Website',
+        link: 'https://xtonyyu.github.io/website-with-custom-carousel/detail.html',
+        github: 'https://github.com/xTonyYu/website-with-custom-carousel',
+        img: './images/PlumPear.png',
+        skillsApplied: ['JavaScript', 'HTML/CSS'],
+        desc: 'Personal project (May 2020): A site that provides potential career information for UX designers, developers, and product mangers.  Main challenges included creating a responsive carousel with cards of various widths and resizable based on viewport width and a search box with type-ahead-suggestion.'
     },
     {
-        css: 'flex-grow',
-        title: "MERN Stack E-commerce Site With Inventory App",
-        link: 'https://plumpear-store.herokuapp.com/login',
-        github: 'https://github.com/xTonyYu/Plum-and-Pear-client',
-        img: './images/plumPearStore.png',
-        skillsApplied: ['React', 'Express', 'MongoDB/Mongoose', 'Node.js', 'Payment API', 'Project planning'],
-        desc: 'A responsive full stack e-commerce site that served both the customers and shop owners.  Functionality included: RESTful Node server API; full CRUD on the inventory; utilized bcrypt and JSON web token to secure authentication and authorization of login and register; customers able to save shopping cart items until next session and make purchase using Stripe payment API. <em>(ID: "customer" Password: "1234"; ID: "AdminX" Password: "2wsx")</em>'
+        css: 'justify-content',
+        title: "90's Tamagotchi Game",
+        link: 'https://xtonyyu.github.io/Tamagotchi-Game/',
+        github: 'https://github.com/xTonyYu/Tamagotchi-Game',
+        img: './images/tamagotchi-night.png',
+        skillsApplied: ['JavaScript', 'jQuery', 'HTML/CSS', 'Responsive design'],
+        desc: 'A fast-pace online game inspired by 90’s Tamagotchi game.  The object of the game is to keep your Tamagotchi alive by feeding it, playing with it, and making sure it takes naps.  However, taking naps will slowly make Tamagotchi hungry and it can only be fed while it’s awake.'
     },
+    
 ]
